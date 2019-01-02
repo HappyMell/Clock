b767b2e06b4443e738bdb9969c537929f60e98a7
HappyMell <mellisamadrid@hotmail.com>
2019-01-02 15:52:38 +0000

    check for errors and correct

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const secondHand = document.querySelector('.second-hand');
 const bigHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 
-function setDate(){
+function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const minute = now.getMinutes();
-    const hour = now.getHours();
-    const hourHand = ((hour / 12) *360) + 90;
     const secondDegrees = ((seconds / 60) * 360) + 90;
-    const minuteHands = ((minute / 60) * 360) + 90;
+
+
+    const minute = now.getMinutes();
+    const minuteHands = ((minute / 60) * 360) + ((seconds / 60) * 6) + 90;
+
+    const hour = now.getHours();
+    const hourHand = ((hour / 12) * 360) + ((minute / 60) * 30) + 90;
+
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
     bigHand.style.transform = `rotate(${hourHand}deg)`;
 
-    minuteHand.style.transform = `rotate(${minuteHands} deg)`;
+    minuteHand.style.transform = `rotate(${minuteHands}deg)`;
 
 }
 
