21e999a597890adcb9a021d36e5da596ade638dd
HappyMell <mellisamadrid@hotmail.com>
2019-01-02 15:35:38 +0000

    edited JS

--- dist/script.js
+++ dist/script.js
@@ -1,20 +1,25 @@
-const secondHand = document.querySelector('.second-hand');
-const bigHand = document.querySelector('.hour-hand');
-const minuteHand = document.querySelector('.min-hand');
 
-function setDate(){
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const bigHand = document.querySelector('.hour-hand');
+
+function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const minute = now.getMinutes();
-    const hour = now.getHours();
-    const hourHand = ((hour / 12) *360) + 90;
     const secondDegrees = ((seconds / 60) * 360) + 90;
-    const minuteHands = ((minute / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+
+    const minute = now.getMinutes();
+    const minuteHands = ((minute / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minuteHand.style.transform = `rotate(${minuteHands}deg)`;
+
+    const hour = now.getHours();
+    const hourHand = ((hour / 12) * 360) + ((minute / 60) * 6) + 90;
     bigHand.style.transform = `rotate(${hourHand}deg)`;
 
-    minuteHand.style.transform = `rotate(${minuteHands} deg)`;
 
 }
 
